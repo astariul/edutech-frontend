26f4c0349cdfa895610c4a6f4c42a047a52ec84a
Astariul <remondnicola@gmail.com>
2022-05-31 23:59:30 +0900

    ✨ Add hero header

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import HeaderResponsive from './components/Header';
 import FooterSimple from './components/Footer';
+import HeroContentLeft from './components/HeroHeader';
 import { MantineProvider } from '@mantine/core';
 
 function App() {
   return (
     <MantineProvider>
       <HeaderResponsive />
+      <HeroContentLeft />
       <FooterSimple />
     </MantineProvider>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -122,7 +122,7 @@ export default function HeaderResponsive() {
   ));
 
   return (
-    <Header height={HEADER_HEIGHT} mb={120} className={classes.root}>
+    <Header height={HEADER_HEIGHT} className={classes.root}>
       <Container className={classes.header}>
         <Group spacing={5} className={classes.links}>
           <Book />
