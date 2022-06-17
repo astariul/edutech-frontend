81a255eb4743056636d216c96cd23e195ab82f77
Astariul <remondnicola@gmail.com>
2022-06-17 09:24:05 +0900

    :recycle: Refactor code to use AppShell

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import HeaderResponsive from './components/Header';
-import FooterSimple from './components/Footer';
 import Home from './pages/Home';
 import Courses from './pages/Courses';
-import Community from './pages/Community';
-import { MantineProvider } from '@mantine/core';
-import { Routes, Route } from 'react-router-dom';
+import UserPage from './pages/UserPage';
+import { MantineProvider, AppShell } from '@mantine/core';
+import { useBooleanToggle } from '@mantine/hooks';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import HeaderResponsive from './components/Header';
+import FooterSimple from './components/Footer';
+import NavbarMinimal from './components/VerticalNavBar';
 
-const RoutesList = () => {
-  return (
-    <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/courses' element={<Courses />}></Route>
-      <Route path='/community' element={<Community />}></Route>
-    </Routes>
-  );
-}
 
 function App() {
+  const [navOpened, toggleNavOpened] = useBooleanToggle(false);
+
+  const location = useLocation();
+  useEffect(() => {
+    if (location.pathname === "/mypage") {
+      toggleNavOpened(true);
+    } else {
+      toggleNavOpened(false);
+    }
+  }, [location.pathname ]);
+
+  let navbar;
+  if (navOpened) {
+    navbar = <NavbarMinimal />;
+  } else {
+    navbar = undefined;
+  };
+
   return (
     <MantineProvider>
-      <HeaderResponsive />
-      <RoutesList />
-      <FooterSimple />
+      <AppShell
+        navbar={navbar}
+        header={<HeaderResponsive />}
+        footer={<FooterSimple />}
+        styles={(theme) => ({
+          main: { padding: '0px' },
+        })}
+      >
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/courses' element={<Courses />}></Route>
+          <Route path='/mypage' element={<UserPage />}></Route>
+        </Routes>
+      </AppShell>
     </MantineProvider>
   );
 }
