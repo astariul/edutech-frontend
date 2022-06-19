fd75b3f30f3994551e3e5aeb8e8f337abbed5d03
Astariul <remondnicola@gmail.com>
2022-06-19 22:17:02 +0900

    :art: Better style for logout button

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,8 +1,25 @@
-import { Button, Tooltip } from '@mantine/core';
+import { createStyles, Button, Tooltip } from '@mantine/core';
 import { useLocalStorage } from '@mantine/hooks';
 import { Logout } from 'tabler-icons-react';
 
+const useStyles = createStyles((theme) => ({
+  logoutbtn: {
+    width: 50,
+    height: 50,
+    borderRadius: theme.radius.md,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+
+    '&:hover': {
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[0],
+    },
+  },
+}));
+
 export default function LogoutButton() {
+  const { classes, cx } = useStyles();
   const [, setLoginJwt] = useLocalStorage<string | null>({ key: 'login-jwt', defaultValue: null });
 
   return (
@@ -11,7 +28,7 @@ export default function LogoutButton() {
       position="bottom"
       withArrow
     >
-      <Button variant="subtle" color="gray" onClick={() => {setLoginJwt(null)}}>
+      <Button className={classes.logoutbtn} variant="subtle" color="gray" onClick={() => {setLoginJwt(null)}}>
         <Logout />
       </Button>
     </Tooltip>
