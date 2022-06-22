e3ea0643bb26b9c5a1a3a7b95cbc671fed7b93b9
Astariul <remondnicola@gmail.com>
2022-06-22 09:31:59 +0900

    :sparkles: Add interface to change profile pic in settings

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -10,6 +10,11 @@ import {
   Text,
   Button,
   Title,
+  Tooltip,
+  UnstyledButton,
+  Avatar,
+  Grid,
+  Space,
 } from '@mantine/core';
 
 export default function Settings() {
@@ -18,10 +23,12 @@ export default function Settings() {
   const [errorProfile, setErrorProfile] = useState<string | null>(null);
   const [errorPassword, setErrorPassword] = useState<string | null>(null);
 
+  const avatar = "https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80";
+
   const formProfile = useForm({
     initialValues: {
-      firstName: 'Jean',
-      lastName: 'Marie',
+      firstName: 'Henry',
+      lastName: 'Silkeater',
       email: 'user@gmail.com',
     },
 
@@ -72,26 +79,39 @@ export default function Settings() {
     }, 500);
   };
 
+  const updateAvatar = () => {
+
+  };
+
   return (
     <Container size="xs" mt={75}>
       <Title order={2} pt="xl" pb="md">
         Public profile
       </Title>
       <form onSubmit={formProfile.onSubmit(saveChanges)}>
-        <Group grow>
-          <TextInput
-            data-autofocus
-            description="You can change your first name"
-            label="First name"
-            {...formProfile.getInputProps('firstName')}
-          />
-
-          <TextInput
-            description="You can change your last name"
-            label="Last name"
-            {...formProfile.getInputProps('lastName')}
-          />
-        </Group>
+        <Grid>
+          <Grid.Col span={6}>
+            <TextInput
+              data-autofocus
+              description="You can change your first name"
+              label="First name"
+              {...formProfile.getInputProps('firstName')}
+            />
+            <Space h="xs" />
+            <TextInput
+              description="You can change your last name"
+              label="Last name"
+              {...formProfile.getInputProps('lastName')}
+            />
+          </Grid.Col>
+          <Grid.Col offset={1} span={4} py="xl">
+            <Tooltip label="Edit" position="right" withArrow transitionDuration={0}>
+              <UnstyledButton onClick={updateAvatar}>
+                <Avatar size={150} src={avatar} radius={150} />
+              </UnstyledButton>
+            </Tooltip>
+          </Grid.Col>
+        </Grid>
 
         <TextInput
           mt="md"
