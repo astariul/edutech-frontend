74540b08f419c51b681dbbd7eae699c3f5f5d6f4
JunPyoL22 <jplee031107@gmail.com>
2022-09-13 22:19:38 +0900

    :bug: npx command 에러 수정

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "> FE 배포"
 # sudo cp -rf /home/ubuntu/supercoding-front/dist/* /var/www/html
+source /home/ubuntu/.profile
 cd /home/ubuntu/supercoding-front
-$(which npx) --yes serve -s build -l 3000
+npx --yes serve -s build -l 3000
