6580a591101100e52391fd58fa86cb9eda6a8d7b
JunPyoL22 <jplee031107@gmail.com>
2022-09-13 21:11:32 +0900

    :bug: npx 커맨드를 찾지못하는 에러 수정

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,4 @@
 echo "> FE 배포"
 # sudo cp -rf /home/ubuntu/supercoding-front/dist/* /var/www/html
 cd /home/ubuntu/supercoding-front
-npxpath=$(which npx)
-echo $npxpath
 $(which npx) --yes serve -s build -l 3000
