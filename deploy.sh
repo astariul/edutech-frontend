171a997ccddcd92dea2d6b355881bd1c876d9ed1
JunPyoL22 <jplee031107@gmail.com>
2022-09-13 20:28:00 +0900

    :bug: deploy.sh npx command not fond 에러 수정

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,6 @@
 echo "> FE 배포"
 # sudo cp -rf /home/ubuntu/supercoding-front/dist/* /var/www/html
 cd /home/ubuntu/supercoding-front
-npx --yes serve -s build -l 3000
+npxpath=$(which npx)
+echo $npxpath
+$(which npx) --yes serve -s build -l 3000
