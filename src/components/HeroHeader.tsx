ce8935ff1eb0a7fd05db33ea5f0324ab61ae670d
hychoi <jjanggu1993@gmail.com>
2022-09-29 00:33:29 +0900

    Replace template images with santa images

--- src/components/HeroHeader.tsx
+++ src/components/HeroHeader.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createStyles, Overlay, Container, Text } from '@mantine/core';
+import hero_image from "../img/visual.jpg"
 
 const useStyles = createStyles((theme) => ({
   hero: {
     position: 'relative',
     backgroundImage:
-      'url(https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)',
+      `url(${hero_image})`,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
   },
@@ -70,7 +71,7 @@ export default function HeroContentLeft() {
     <div className={classes.hero}>
       <Overlay
         gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 40%)"
-        opacity={1}
+        opacity={.5}
         zIndex={0}
       />
       <Container className={classes.container}>
--- src/components/first_component.tsx
+++ src/components/first_component.tsx
@@ -1,29 +1,31 @@
 import { createStyles, SimpleGrid, Card, Image, Text, Container, AspectRatio, Transition } from '@mantine/core';
 import { useWindowScroll } from '@mantine/hooks';
 
+import thumb_image from "../img/pd_thumb_hover.jpg"
+
 const mockdata = [
   {
     title: '30일 베이직 패키지',
     image:
-      'https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+      thumb_image,
     price: '가격',
   },
   {
     title: '60일 베이직 패키지',
     image:
-      'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+      thumb_image,
     price: '가격',
   },
   {
     title: '90일 베이직 패키지',
     image:
-      'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+      thumb_image,
     price: '가격',
   },
   {
     title: '120일 베이직 패키지',
     image:
-      'https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80',
+      thumb_image,
     price: '가격',
   },
 ];
@@ -31,6 +33,8 @@ const mockdata = [
 const useStyles = createStyles((theme) => ({
   card: {
     transition: 'transform 150ms ease, box-shadow 150ms ease',
+    width: '270px',
+    height: '380px',
 
     '&:hover': {
       transform: 'scale(1.01)',
@@ -41,6 +45,7 @@ const useStyles = createStyles((theme) => ({
   title: {
     fontFamily: `Greycliff CF, ${theme.fontFamily}`,
     fontWeight: 600,
+    fontsize: theme.fontSizes.lg,
   },
 
   suptitle: {
@@ -61,23 +66,23 @@ export function ArticlesCardsGrid() {
   const [scroll, scrollTo] = useWindowScroll();
 
   const cards = mockdata.map((article) => (
-    <Card key={article.title} p="sm" radius="md" component="a" href="#" className={classes.card} withBorder={true}>
-      <AspectRatio ratio={1}>
-        <Image src={article.image} radius="md" />
+    <Card key={article.title} p={0} radius="md" component="a" href="#" className={classes.card} withBorder={true}>
+      <AspectRatio ratio={1 / 1}>
+        <Image src={article.image} radius="md" m={0} p={0} />
       </AspectRatio>
-      <Text className={classes.title} mt={5}>
+      <Text className={classes.title} m="lg">
         {article.title}
       </Text>
-      <Text color="dimmed" size="xs" transform="uppercase" weight={700} mt="md">
+      <Text color="dimmed" size="xs" transform="uppercase" weight={700} m="lg">
         {article.price}
       </Text>
     </Card>
   ));
 
   return (
-    <Container py="xl" size="lg">
+    <Container py="xl" size="lg" mt={20} style = {{height: '700px'}}>
       <Transition mounted={scroll.y > 10} transition={left} timingFunction="ease" duration={5000}>
-       {(styles) => <Text py="xl" className={classes.suptitle}>슈퍼 코딩 프리미엄 요금제</Text>}
+       {(styles) => <Text my="xl" className={classes.suptitle}>슈퍼 코딩 프리미엄 요금제</Text>}
       </Transition>
       <SimpleGrid cols={4} breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
         {cards}
