737be4872bfe430dd6ec162489c60487dda5af7e
JunPyoL22 <jplee031107@gmail.com>
2022-10-01 01:16:06 +0900

    fix: Course 페이지 경로 수정

--- src/pages/course/Course.tsx
+++ src/pages/course/Course.tsx
@@ -1,17 +1,18 @@
 import { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import CourseStatusBox from '../components/courseStatusBox/CourseStatusBox';
-import { ICourse, IVideo, ICourseVideo } from '../typings/db';
-import CourseRepository from '../repositories/Course';
+import CourseStatusBox from '../../components/courseStatusBox/CourseStatusBox';
+import { ICourse, IVideo, ICourseVideo } from '../../typings/db';
+import CourseRepository from '../../repositories/Course';
 import { useLocalStorage } from '@mantine/hooks';
 import { useNavigate } from "react-router-dom";
-import { IUserProfile } from '../typings/db';
-import { Button, Center, Grid, } from '@mantine/core';
+import { IUserProfile } from '../../typings/db';
+import { Button, Center } from '@mantine/core';
 import { Square, SquareCheck } from 'tabler-icons-react';
-import { findUniqueSeasonNumber, secondsToMinutesString } from '../utils/common';
-import AuthenticationForm from '../components/AuthentificationForm';
-import ToggleTable from '../components/Table';
+import { findUniqueSeasonNumber, secondsToMinutesString } from '../../utils/common';
+import AuthenticationForm from '../../components/AuthentificationForm';
+import ToggleTable from '../../components/Table';
 import { useCallback } from 'react';
+import useStyles from './style';
 
 
 const Course = () => {
@@ -21,33 +22,8 @@ const Course = () => {
   const [formType, setFormType] = useState<"register" | "login">("login");
   const {course, videos} = location.state as ICourseVideo;
   const isCompletes = useRef<{[key: string]:string}>({});
-  const seasons = findUniqueSeasonNumber(videos as IVideo[]);
-
-  useEffect(
-    () => {
-      if (!videos) {
-        console.log(videos);
-      }
-      else {
-        (videos as IVideo[]).forEach(
-          (video) => {
-            new CourseRepository()
-              .isCompletedEpisode(login?.token as string, course.id, video.number)
-              .then(
-                (data) => {
-                  isCompletes.current = {
-                    ...isCompletes.current,
-                    [`EP${video.number}`]: data,
-                  }
-                }
-              )
-            }
-            )
-      }
-      return () => {
-        isCompletes.current = {}
-      }
-  }, [login, course, videos, isCompletes]);
+  const [table, setTable] = useState<JSX.Element | JSX.Element[]>();
+  const {classes} = useStyles();
 
   const onResumeCourse = (course: ICourse, videos: IVideo[] | { message: string }) => {
     new CourseRepository()
@@ -108,21 +84,22 @@ const Course = () => {
     }, [navigate]
   )
 
-  const generateTable = (seasons: number[], videos: IVideo[] | { message: string }) => {
+  const generateTable = useCallback(
+    (videos: IVideo[] | { message: string }) => {
     if (videos.hasOwnProperty("message")){
       return (
         <div>강의 영상 업로드전 입니다.</div>
       )
     }
     return (
-      (seasons).map(
+      findUniqueSeasonNumber(videos as IVideo[]).map(
         (season) => {
           const rows = (videos as IVideo[]).map(
             (video) => {
               return (video.seasonNumber === season) && (
                   <tr key={video.number}>
-                    <td style={{cursor: "pointer"}} onClick={() => {navigateToClassRoom(course, video)}}>EP{video.number}</td>
-                    <td style={{cursor: "pointer"}} onClick={() => {navigateToClassRoom(course, video)}}>{video.title}</td>
+                    <td>EP{video.number}</td>
+                    <td>{video.title}</td>
                     <td>{secondsToMinutesString(video.duration)}</td>
                     <td>
                       {
@@ -133,36 +110,66 @@ const Course = () => {
                         )
                       }
                     </td>
-                    <td>{""}</td>
+                    <td style={{cursor: "pointer"}} onClick={() => {navigateToClassRoom(course, video)}}>{"에피소드 시청하기"}</td>
                   </tr>
                 )
               }
             )
           return (
-            <Grid style={{margin: "10px 350px", flexDirection: "column", justifyContent: "space-between"}}>
+            <div className={classes.table}>
               <ToggleTable
-                header={[`시즌 ${season}`, "시즌 제목", "길이", `수강완료`]}
+                header={[`시즌 ${season}`, "시즌 제목", "길이", "수강완료"]}
                 rows={rows as JSX.Element[]}
               />
-            </Grid>
+            </div>
           )
         }
       )
     )
-  }
+  }, [course, navigateToClassRoom, classes]
+  )
+
+  useEffect(
+    () => {
+      if (!videos) {
+        console.log(videos);
+      }
+      else {
+        (videos as IVideo[]).forEach(
+          (video) => {
+            new CourseRepository()
+              .isCompletedEpisode(login?.token as string, course.id, video.number)
+              .then(
+                (data) => {
+                  isCompletes.current = {
+                    ...isCompletes.current,
+                    [`EP${video.number}`]: data,
+                  };
+                  setTable(generateTable(videos));
+                }
+              )
+            }
+            )
+      }
+      return () => {
+        isCompletes.current = {}
+      }
+  }, [login, course, videos, isCompletes, setTable, generateTable]);
 
   return (
     <>
       {
         (login) && (
-          <>
+          <div className={classes.main}>
             <CourseStatusBox
               course={course}
               videos={videos}
               generateButtonGroup={() => getbuttonGroup(course, videos)}
             />
-            {generateTable(seasons, videos)}
-        </>
+            <div className={classes.tableContainer}>
+              {table}
+            </div>
+          </div>
         )
       }
       {
---
+++ src/pages/course/style.ts
@@ -0,0 +1,22 @@
+import { createStyles } from "@mantine/core";
+
+const useStyles = createStyles(() => ({
+  main: {
+    margin: "20px auto",
+    maxWidth: "1248px",
+  },
+  tableContainer: {
+    margin: "0 auto",
+    padding: "12px 100px",
+  },
+  table: {
+    boxSizing: "border-box",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  }
+
+}));
+
+export default useStyles;
