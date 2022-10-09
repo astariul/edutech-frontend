7d9dc5ae07a70b90d2d777c85360bc6b87843a4a
JunPyoL22 <jplee031107@gmail.com>
2022-10-10 04:11:13 +0900

    Feat: AuthRepository login 메서드 추가

--- src/repositories/Auth.tsx
+++ src/repositories/Auth.tsx
@@ -4,14 +4,23 @@ import { IMe } from "../typings/db";
 
 class AuthRepository {
 
-    async me(token: string) {
-        const path = process.env.REACT_APP_API_URL + "auth/me"
-        const ret = await axios.get<IMe>(
-            path,
-            { headers: {Authorization: `Bearer ${token}`} }
-        );
-        return ret;
-    }
+  async login(id: string, password: string) {
+    const path = process.env.REACT_APP_API_URL + "auth/login"
+    const ret = await axios.post(
+      path,
+      { email: id, password: password },
+    );
+    return ret;
+  }
+
+  async me(token: string) {
+      const path = process.env.REACT_APP_API_URL + "auth/me"
+      const ret = await axios.get<IMe>(
+          path,
+          { headers: {Authorization: `Bearer ${token}`} }
+      );
+      return ret;
+  }
 }
 
 export default AuthRepository;
