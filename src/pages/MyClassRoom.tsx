c85c39f0d8641d583b4fc50e439d69ea95ca6f22
JunPyoL22 <jplee031107@gmail.com>
2022-10-11 20:24:26 +0900

    Fix: 기존 로그인 폼 제거와 관련한 변경에서 미반영사항 추가

--- src/pages/MyClassRoom.tsx
+++ src/pages/MyClassRoom.tsx
@@ -57,7 +57,7 @@ const MyClassRoom = () => {
             setMyCourses(courses);
           }
         )
-    }, [login]
+    }, [login, navigate]
   )
 
   const navigateCourse = (course: ICourse, videos: IVideo[] | { message: string }) => {
