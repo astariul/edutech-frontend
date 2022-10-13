d3a5e0ff46d80999e632e0a0de3a5f41045b28c3
JunPyoL22 <jplee031107@gmail.com>
2022-10-14 00:02:00 +0900

    Refactor: 결제방식 모달 컴포넌트 인터페이스 수정 - 결제 유저플로우에서 PG사 선택이 추가되면서 변경된 결제플로우를 유연하게 구현하기 위한 구조로 변경 - PG사에 따라 상의한 결제방식을 지원함에 따라, PG사별 결제방식을 다르게 하기 위한 더 좋은 구조로 변경

--- src/components/paymentMethodModal/PaymentMethodModal.tsx
+++ src/components/paymentMethodModal/PaymentMethodModal.tsx
@@ -1,88 +1,25 @@
 import { Button, Modal, Grid } from '@mantine/core';
-import { useLocalStorage } from "@mantine/hooks";
-import { Buyer, IOrder, IUserProfile, MyOrder } from '../../typings/db';
-import OrderRepository from '../../repositories/Order';
 import useStyles from "./style";
 
 
-const { IMP } = window;
-IMP.init(process.env.REACT_APP_IAMPORT_ID);
-
 interface PaymentMethodModalProps {
-  order: MyOrder;
-  buyer: Buyer;
-  paymentMethods: string[];
+  paymentMethods: string[]; //결제방식
+  paymentMethodCodeMap: Map<string, string>; //결제 방식명과 결제방식 iamport 코드 Map
   opened: boolean;
   modalCloser: (close: boolean) => void;
-  onSuccessHandler?: (order: IOrder) => void;
-  onFailHandler?: (response: any) => void;
+  onSelectItem: (item: string) => void;
 }
 
 const PaymentMethodModal = (
   {
-    order,
-    buyer,
     paymentMethods,
+    paymentMethodCodeMap,
     opened,
     modalCloser,
-    onSuccessHandler = (order: IOrder) => {},
-    onFailHandler = () => {},
+    onSelectItem,
   }: PaymentMethodModalProps
 ) => {
-  const [login] = useLocalStorage<IUserProfile | null>({ key: "login", defaultValue: null });
   const { classes } = useStyles();
-
-  const payCallback = (response: any) => {
-    if (response.success) {
-      new OrderRepository()
-      .completeOrderById(login?.token as string, order.orderId, response.imp_uid)
-      .then(
-        (myorder) => {
-          // Note: onSuccessHandler가 결제 성공시 로직을 담당한다.
-          // TODO myorder 데이터로 가상계좌 발급을 통한 결제성공을 구분할 수 없음. 백엔드 협의 후 수정 필요
-          onSuccessHandler(myorder);
-        })
-    } else {
-      // Note: onFailHandler이 결제 실패 로직을 담당한다.
-      onFailHandler(response);
-    }
-  };
-  const requestPayment = (order: MyOrder, buyer: Buyer, method: string) => {
-    let payMethod;
-    const digital = true;
-    // (참고) 결제수단 구분 코드 : https://chai-iamport.gitbook.io/iamport/sdk/javascript-sdk/payrq
-    switch (method) {
-      case "신용카드":
-        payMethod = "card";
-        break;
-      case "실시간계좌이체":
-        payMethod = "trans";
-        break;
-      case "가상계좌":
-        payMethod = "vbank";
-        break;
-      case "휴대폰소액결제":
-        payMethod = "phone";
-        break;
-    }
-    if (login) {
-      IMP.request_pay({
-        pg: "html5_inicis", //이니시스웹표준
-        pay_method: payMethod,
-        merchant_uid: order.orderId,
-        name: order.productName,
-        amount: order.dcPrice,
-        buyer_email: buyer.email,
-        buyer_name: buyer.name,
-        buyer_tel: buyer.tel,
-        currency: "KRW",
-        digital: digital
-      }, payCallback)
-    } else {
-      // TODO: 디자인된 Modal 적용
-      alert("결제를 진행하려면 로그인이 필요합니다.")
-    }
-  }
 
   return (
     <Modal
@@ -94,16 +31,21 @@ const PaymentMethodModal = (
       <Grid className={classes.modal}>
         {
           paymentMethods.map(
-            (method, index) => { 
+            (payMethod, index) => {
               return (
                 <Button
                   key={index}
                   className={classes.button}
                   variant="outline"
-                  onClick={() => { modalCloser(true); requestPayment(order, buyer, method)}}
+                  onClick={
+                    () => {
+                      modalCloser(true);
+                      onSelectItem(paymentMethodCodeMap.get(payMethod) as string);
+                    }
+                  }
                   type="button"
                 >
-                  {method}
+                  {payMethod}
                 </Button>
               )
             }
