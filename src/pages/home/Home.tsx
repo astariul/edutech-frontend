3e50b4bb39eafce8f2485701bf52488fac7a66ee
JunPyoL22 <jplee031107@gmail.com>
2022-10-21 00:23:55 +0900

    Fix: Hone 디자인 시안 적용

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,22 @@
+
+import SuperCourse from "../../components/home/SuperCourse";
+import SuperCurriculum from "../../components/home/SuperCurriculum";
+import SuperResume from "../../components/home/SuperResume";
+import SuperManagement from "../../components/home/SuperManagement";
+import SuperReivew from "../../components/home/SuperReview";
+import SuperCommunity from "../../components/home/SuperCommunity";
+
+const Home = () => {
+  return (
+    <>
+      <SuperCourse />
+      <SuperCurriculum />
+      <SuperResume />
+      <SuperManagement />
+      <SuperReivew />
+      <SuperCommunity />
+    </>
+  );
+}
+
+export default Home;
