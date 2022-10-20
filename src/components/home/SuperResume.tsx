95f0627c40d47d0bfdef7b3f32549a5d66accce0
JunPyoL22 <jplee031107@gmail.com>
2022-10-21 00:22:04 +0900

    Fix: 이미지명 수정

--- src/components/home/SuperResume.tsx
+++ src/components/home/SuperResume.tsx
@@ -6,7 +6,7 @@ const SuperResume = () => {
   return (
     <div className={classes.main}>
       <div className={classes.textArea}>
-        <div className={classes.category}>SUPER REVIEW</div>
+        <div className={classes.category}>SUPER RESUME</div>
         <div className={classes.titleMobile}>
           업계 최고의 CTO 추천서를
           <br />
@@ -21,12 +21,12 @@ const SuperResume = () => {
         </div>
       </div>
       <div className={classes.cardArea}>
-        <img className={classes.cardLeft} src={"/image/home/superReviewCardLeft.png"} alt={""}/>
-        <img className={classes.cardLeftMobile} src={"/image/home/superReviewCardLeftMobile.png"} alt={""}/>
-        <img className={classes.cardMiddle} src={"/image/home/superReviewCardMiddle.png"} alt={""}/>
-        <img className={classes.cardMiddleMobile} src={"/image/home/superReviewCardMiddleMobile.png"} alt={""}/>
-        <img className={classes.cardRight} src={"/image/home/superReviewCardRight.png"} alt={""}/>
-        <img className={classes.cardRightMobile} src={"/image/home/superReviewCardRightMobile.png"} alt={""}/>
+        <img className={classes.cardLeft} src={"/image/home/superResumeCardLeft.png"} alt={""}/>
+        <img className={classes.cardLeftMobile} src={"/image/home/superResumeCardLeftMobile.png"} alt={""}/>
+        <img className={classes.cardMiddle} src={"/image/home/superResumeCardMiddle.png"} alt={""}/>
+        <img className={classes.cardMiddleMobile} src={"/image/home/superResumeCardMiddleMobile.png"} alt={""}/>
+        <img className={classes.cardRight} src={"/image/home/superResumeCardRight.png"} alt={""}/>
+        <img className={classes.cardRightMobile} src={"/image/home/superResumeCardRightMobile.png"} alt={""}/>
       </div>
     </div>
   )
