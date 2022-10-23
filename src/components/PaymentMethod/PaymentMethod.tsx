e8857458ec0be088e886cd57a54d307217f20a1b
DanielLove23 <lkylove2323@gmail.com>
2022-10-23 17:08:01 +0900

    Feat: 결제 버튼 셋업

--- src/components/PaymentMethod/PaymentMethod.tsx
+++ src/components/PaymentMethod/PaymentMethod.tsx
@@ -1,7 +1,14 @@
+import useStyles from "./style";
+
 const PaymentMethod=()=>{
+    const {classes, cx} = useStyles();
     return(
         <div>
-            Payment Method
+            <div className={classes.container} style={{padding:"2% 10%"}}>
+            <h2>결제 방식</h2>
+            <button>KG 이니시스</button>
+            </div>
+
         </div>
     )
 };
---
+++ src/components/PaymentMethod/style.ts
@@ -0,0 +1,13 @@
+import { createStyles } from "@mantine/core";
+const useStyles = createStyles(() => ({
+    cart: {
+      width: "100%",
+      marginRight: "32px",
+      marginBottom: "120px",
+    },
+    container:{
+        display:"flex",
+    }
+}));
+
+export default useStyles;
