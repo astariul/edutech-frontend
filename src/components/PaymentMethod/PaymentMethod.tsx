e85af90d74bd9b8387d30ceb40952f701e038601
lkylove23 <lkylove2323@gmail.com>
2022-10-23 18:44:50 +0900

    Feat: Buttons with Mantine

--- src/components/PaymentMethod/PaymentMethod.tsx
+++ src/components/PaymentMethod/PaymentMethod.tsx
@@ -1,16 +1,29 @@
 import useStyles from "./style";
+import { Button } from "@mantine/core";
 
-const PaymentMethod=()=>{
-    const {classes, cx} = useStyles();
-    return(
+const PaymentMethod = () => {
+  const { classes, cx } = useStyles();
+  return (
+    <div>
+      <h2 style={{ padding: "2% 10% 0% 10%" }}>결제 방식</h2>
+      <div className={classes.container} style={{ padding: "0% 10%" }}>
         <div>
-            <div className={classes.container} style={{padding:"2% 10%"}}>
-            <h2>결제 방식</h2>
-            <button>KG 이니시스</button>
-            </div>
-
+          <div style={{ width: 200 }}>
+            <Button variant="outline" color="gray">
+              KG 이니시스
+            </Button>
+          </div>
+          <button>t</button>
+          <Button variant="outline" color="gray">
+            카카오페이
+          </Button>
+          <Button variant="outline" color="gray">
+            토스
+          </Button>
         </div>
-    )
+      </div>
+    </div>
+  );
 };
 
 export default PaymentMethod;
--- src/components/PaymentMethod/style.ts
+++ src/components/PaymentMethod/style.ts
@@ -1,13 +1,14 @@
 import { createStyles } from "@mantine/core";
 const useStyles = createStyles(() => ({
-    cart: {
-      width: "100%",
-      marginRight: "32px",
-      marginBottom: "120px",
-    },
-    container:{
-        display:"flex",
-    }
+  cart: {
+    width: "100%",
+    marginRight: "32px",
+    marginBottom: "120px",
+  },
+  container: {
+    display: "flex",
+    flexDirection: "row",
+  },
 }));
 
 export default useStyles;
