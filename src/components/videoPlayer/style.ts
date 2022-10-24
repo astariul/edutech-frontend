e9f913ddf35d79c60fda9a06225c4113f0619308
JunPyoL22 <jplee031107@gmail.com>
2022-10-24 14:53:05 +0900

    Feat:: Video 컴포넌트 추가

---
+++ src/components/videoPlayer/style.ts
@@ -0,0 +1,11 @@
+import { createStyles } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  playIcon: {
+    display: "flex",
+    marginLeft: "auto",
+    marginRight: "auto"
+  }
+}));
+
+export default useStyles;
