3fd26c0d548f3f94b1a07ca150f31367339671ae
JunPyoL22 <jplee031107@gmail.com>
2022-10-28 01:18:17 +0900

    Design: 상품페이지 Body(프로그램) 파트 반응형 디자인 적용

--- src/components/product/BodyProgram.tsx
+++ src/components/product/BodyProgram.tsx
@@ -41,6 +41,12 @@ const BodyProgram = () => {
         <div>
           <h2>why? <br/>슈퍼코딩 웹개발 종합반을 선택해야 할까요?</h2>
         </div>
+        <div className="mobile">
+          <h2>why?
+            <br/>슈퍼코딩 웹개발 종합반을
+            <br />선택해야 할까요?
+          </h2>
+        </div>
       </section>
       <section className={classes.main}>
         <div className={classes.contents}>
@@ -62,6 +68,34 @@ const BodyProgram = () => {
           <div className={classes.answer}>
             <h2>
               업계 최고의 CTO추천서를 부여받는 곳은 <br/>
+              <span>'슈퍼코딩'</span>유일하기 때문입니다.
+            </h2>
+          </div>
+        </div>
+        <div className={classes.contentsMobile}>
+          <div className={classes.answerMobile}>
+            <h2>
+              기업이 원하는 인재를 만들기 위한
+              <br /><span>100인의 업계 최고들의</span>
+              <br /><span>적극적인 참여</span>로 프로그램을 완벽하게
+              <br />완성시켰기 때문입니다.
+            </h2>
+          </div>
+          <div className={classes.superMobile}>
+            <div>SUPER</div>
+            <h2>
+                30인+팀장급 리더개발자님들이 원하는
+                <br />프로그램으로 구성되어 있습니다.
+            </h2>
+          </div>
+          <section className={classes.programMobile}>
+            <TransitionProgram />
+          </section>
+          <div className={classes.answerMobile}>
+            <h2>
+              기업이업계 최고의
+              <br />CTO추천서를 부여받는 곳은
+              <br/>
               <span>'슈퍼코딩'</span>유일하기 때문입니다.
             </h2>
           </div>
--- src/components/product/bodyProgramStyle.ts
+++ src/components/product/bodyProgramStyle.ts
@@ -8,6 +8,11 @@ const useBodyProgramStyles = createStyles((theme) => ({
     fontFamily: "NotoSansKR",
     fontStyle: "normal",
     whiteSpace: "nowrap",
+
+    [`@media (max-width: 376px)`]: {
+      paddingTop: "0px",
+      width: "375px",
+    },
   },
 
   contents: {
@@ -16,6 +21,10 @@ const useBodyProgramStyles = createStyles((theme) => ({
     justifyContent: "center",
     flexDirection: "column",
     
+    [`@media (max-width: 376px)`]: {
+      display: "none",
+    },
+
     "& > div": {
       width: "1168px",
       alignItems: "flex-start",
@@ -34,6 +43,34 @@ const useBodyProgramStyles = createStyles((theme) => ({
     }
   },
 
+  contentsMobile: {
+    display: "none",
+
+    [`@media (max-width: 376px)`]: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+
+      "& > div": {
+        width: "336px",
+        marginTop: "45px",
+      },
+
+      "& > div: nth-of-type(2)":{
+        marginTop: "45px",
+      },
+
+      "& > div: nth-of-type(3)":{
+        marginTop: "45px",
+      },
+
+      "& > section": {
+        marginTop: "37.97px",
+      }
+    },
+  },
+
   message: {
     backgroundImage: "url(/image/product/why.png)",
     backgroundSize: "cover",
@@ -44,12 +81,30 @@ const useBodyProgramStyles = createStyles((theme) => ({
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
-    
-    "& > div": { 
+
+    [`@media (max-width: 376px)`]: {
+      backgroundImage: "url(/image/product/whyMobile.png)",
+      marginTop: "45px",
+      width: "375px",
+      height: "166px",
+    },
+
+    "& > div: nth-of-type(1)": { 
       width: "1168px",
       margin: "0 auto",
+
+      [`@media (max-width: 376px)`]: {
+        display: "none",
+        width: "336px",
+      }
     },
     
+    "& > div.mobile": { 
+      [`@media (max-width: 376px)`]: {
+        width: "336px",
+      }
+    },
+  
     "& > div > h2" : {
       color: "#FFFFFF",
       letterSpacing: "-2px",
@@ -59,6 +114,17 @@ const useBodyProgramStyles = createStyles((theme) => ({
       lineHeight: "50px",
       fontWeight: 700,
       fontSize: "30px",
+      margin: 0,
+    },
+
+    "& > div.mobile > h2" : {
+      color: "#FFFFFF",
+      letterSpacing: "-2px",
+      fontFamily: "NotoSansKR",
+      textAlign: "center",
+      lineHeight: "32px",
+      fontWeight: 700,
+      fontSize: "23px",
       margin: 0,
     }
   },
@@ -76,6 +142,32 @@ const useBodyProgramStyles = createStyles((theme) => ({
         color: "#0094FF",
       },
     },
+
+    [`@media (max-width: 376px)`]: {
+      display: "none"
+    }
+  },
+
+  answerMobile: {
+    display: "none",
+
+    [`@media (max-width: 376px)`]: {
+      display: "block",
+      textAlign: "center",
+
+      "& > h2": {
+        margin: 0,
+        fontWeight: 700,
+        fontSize: "23px",
+        lineHeight: "32px",
+        letterSpacing: "-2px",
+        color: "#363636",
+  
+        "& > span": {
+          color: "#0094FF",
+        },
+      },
+    }
   },
 
   super: {
@@ -83,6 +175,11 @@ const useBodyProgramStyles = createStyles((theme) => ({
     display: "flex",
     flexDirection: "row",
     columnGap: "34px",
+
+    [`@media (max-width: 376px)`]: {
+      display: "none",
+
+    },
 
     "& > div": {
       width: "66px",
@@ -105,7 +202,47 @@ const useBodyProgramStyles = createStyles((theme) => ({
     }
   },
 
-  program: {    
+  superMobile: {
+    display: "none",
+    
+    [`@media (max-width: 376px)`]: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      textAlign: "center",
+      rowGap: "10.5px",
+
+      "& > div": {
+        display: "block",
+        width: "46px",
+        fontFamily: "AlongSanss2",
+        fontWeight: 800,
+        fontSize: "14.27px",
+        lineHeight: "16px",
+        letterSpacing: "-0.51px",
+        color: "#363636",
+        borderBottom: "2.79px solid #363636",
+      },
+  
+      "& > h2": { 
+        display: "block",
+        margin: 0,
+        fontWeight: 700,
+        fontSize: "18px",
+        lineHeight: "27px",
+        letterSpacing: "-1px",
+        color: "#363636",
+      }
+    },
+  },
+
+  program: {
+
+    [`@media max-width(376px)`]: {
+      display: "none",
+    },
+
     "& > button": {
       fontFamily: "NotoSansKR",
       fontWeight: 700,
@@ -131,21 +268,71 @@ const useBodyProgramStyles = createStyles((theme) => ({
     },
   },
 
+  programMobile: {
+    display: "none",
+
+    [`@media (max-width: 376px)`]: {
+      display: "block",
+
+      "& > button": {
+        fontFamily: "NotoSansKR",
+        fontWeight: 700,
+        fontSize: "13px",
+        lineHeight: "37px",
+        background: "linear-gradient(0deg, #0094FF, #0094FF), #363636",
+        width: "336px",
+        height: "60px",
+  
+        "& > div > span": {
+          columnGap: "7px",
+        }
+      },
+  
+      "& > div": {
+        width: "336px",
+        background: "linear-gradient(0deg, #0094FF, #0094FF), #363636",
+        height: "560px",
+        marginTop: "-10px",
+        border: 0,
+        borderBottomLeftRadius: "10px",
+        borderBottomRightRadius: "10px",
+      },
+    }
+  },
+
   programImage: {
     paddingLeft: "130px",
     paddingRight: "100px",
     display: "flex",
     flexDirection: "column",
 
+    [`@media (max-width: 376px)`]: {
+      paddingLeft: "15px",
+      paddingRight: "0px",
+      justifyContent: "center",
+      alignItems: "center"
+    },
+
     "& > img: nth-of-type(1)": {
       width: "968px",
       height: "600px",
+
+      [`@media (max-width: 376px)`]: {
+        width: "300px",
+        height: "300px",
+      }
     },
   
     "& > img: nth-of-type(2)": {
       width: "968px",
       height: "524px",
-      marginTop: "-71px"
+      marginTop: "-71px",
+      
+      [`@media (max-width: 376px)`]: {
+        width: "300px",
+        height: "300px",
+      marginTop: "-37px",
+      }
     }
   },
 
