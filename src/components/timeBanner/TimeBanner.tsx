8f8292bd9f7489b8d592f1b75c46f640bbc75fac
JunPyoL22 <jplee031107@gmail.com>
2022-10-27 14:22:53 +0900

    Feat: 타임배너 컴포넌트 추가

---
+++ src/components/timeBanner/TimeBanner.tsx
@@ -0,0 +1,31 @@
+
+import DDayTimer from '../ddayTimer/DdayTimer';
+import { Button } from '@mantine/core';
+import useStyles from './style';
+
+interface TimeBannerProps {
+  dDay: string,
+  message?: string,
+}
+
+const TimeBanner = ({dDay, message}: TimeBannerProps) => {
+  const {classes} = useStyles();
+  return (
+    <section className={classes.main}>
+      <div className={classes.contents}>
+        <DDayTimer dDay={dDay} interval={1000}/>
+        <div className={classes.message}>
+          <h2>{message}</h2>
+        </div>
+        <Button
+          className={classes.paymentButton}
+          radius={6.47}
+        >
+          지금바로구매
+        </Button>
+      </div>
+    </section>
+  )
+}
+
+export default TimeBanner;
---
+++ src/components/timeBanner/style.ts
@@ -0,0 +1,55 @@
+import { createStyles } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  main: {
+    fontFamily: "NotoSansKR",
+    fontStyle: "normal",
+    whiteSpace: "nowrap",
+    background: "linear-gradient(180deg, #1F1F1F 0%, #000000 100%)",
+    boxShadow: "0px -8px 20px rgba(0, 0, 0, 0.3)",
+    borderRadius: "0px",
+    height: "100px"
+  },
+
+  contents: {
+    // paddingTop: "12px",
+    width: "1440px",
+    height: "100px",
+    margin: "0 auto",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    columnGap: "36px",
+  },
+
+  message: {
+    letterSpacing: "-1px",
+    fontStyle: "normal",
+    fontWeight: 900,
+    lineHeight: "30px",
+    
+    "& > h2": {
+      fontSize: "30px",
+      margin: 0,
+      color: "#0094FF",
+    }
+  },
+
+  paymentButton: {
+    background: "linear-gradient(180deg, #FFFFFF 51.04%, #E2E2E2 87.64%)",
+    width: "268px",
+    height: "88px",
+
+    "& > div > span": {
+      fontFamily: "NotoSansKR",
+      fontStyle: "normal",
+      fontWeight: 900,
+      fontSize: "26px",
+      lineHeight: "24px",
+      color: "#000000",
+      letterSpacing: "-1.29577px",
+    }
+  }
+}));
+
+export default useStyles;
