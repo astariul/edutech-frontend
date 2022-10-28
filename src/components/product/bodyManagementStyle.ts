4131e2effd591343191cda36ae3c2acdee6bb813
JunPyoL22 <jplee031107@gmail.com>
2022-10-29 04:26:25 +0900

    Feat: 상품페이지 바디 management 컴포넌트 모바일 디자인 시안 적용

--- src/components/product/bodyManagementStyle.ts
+++ src/components/product/bodyManagementStyle.ts
@@ -1,15 +1,6 @@
 import { createStyles } from '@mantine/core';
 
 const useBodyManagementStyles = createStyles((theme) => ({
-  main: {
-    paddingLeft: "max(137px, 9.514vw)",
-    paddingRight: "max(135px, 9.375vw)",
-    fontFamily: "NotoSansKR",
-    fontStyle: "normal",
-    color: "#363636",
-    whiteSpace: "nowrap",
-  },
-
   logoTile: {
     marginTop: "233px",
     fontFamily: "NotoSansKR",
@@ -19,17 +10,38 @@ const useBodyManagementStyles = createStyles((theme) => ({
     
     height: "562px",
     backgroundImage: "url(/image/product/logoTile.png)",
+
+    [`@media (max-width: 376px)`]: {
+      display: "none"
+    }
   },
   
+  logoTileMobile: {
+    display: "none",
+
+    [`@media (max-width: 376px)`]: {
+      height: "716.4px",
+      fontFamily: "NotoSansKR",
+      fontStyle: "normal",
+      display: "block",
+      backgroundImage: "url(/image/product/logoTileMobile.png)",
+    }
+  },
+
   management: {
-    position: "absolute",
-    top: "5380px",
+
+    [`@media (max-width: 376px)`]: {
+      display: "none"
+    },
+
+    paddingTop: "190px",
     paddingLeft: "max(235px, 9.514vw)",
     color: "#FFFFFF",
     
     "& > h2": {
       margin: 0
     },
+
     "& > h2: nth-of-type(2)" : {
       fontWeight: 500,
       fontSize: "20px",
@@ -37,9 +49,37 @@ const useBodyManagementStyles = createStyles((theme) => ({
     }
   },
 
+  managementMobile: {
+    display: "none",
+
+    [`@media (max-width: 376px)`]: {
+      display: "block",
+      paddingTop: "156px",
+      // position: "absolute",
+      // top: "6080px",
+      color: "#FFFFFF",
+      marginLeft: "66px",
+
+      "& > h2": {
+        margin: 0,
+        fontSize: "18px",
+        lineHeight: "25px",
+        letterSpacing: "-1.05px",
+      },
+
+      "& > h2: nth-of-type(2)" : {
+        marginTop: "58px",
+        fontWeight: 500,
+        fontSize: "12px",
+        lineHeight: "18px",
+        letterSpacing: "-1px",
+      }
+    },
+  },
+
   message: {
     position: "absolute",
-    top: "5100px",
+    top: "6120px",
     paddingLeft: "max(137px, 9.514vw)",
 
     "& > h2:nth-of-type(1)" : {
@@ -64,66 +104,41 @@ const useBodyManagementStyles = createStyles((theme) => ({
       lineHeight: "38px",
       letterSpacing: "-1px",    
   },
+},
+
+  messageMobile: {
+    display: "none",
+
+    [`@media (max-width: 376px)`]: {
+      width: "375px",
+      display: "block",
+      paddingTop: "28px",
+      textAlign: "center",
   
+      "& > h2:nth-of-type(1)" : {
+        margin: 0,
+        whiteSpace: "nowrap",
+        fontWeight: 700,
+        fontSize: "23px",
+        lineHeight: "32px",
+        letterSpacing: "-2px",
 
-  resume: {
-    backgroundColor: "#F2F2F2",
-    fontFamily: "NotoSansKR",
-    fontStyle: "normal",
-    color: "#363636",
-  },
-
-  super: {
-    paddingTop: "45.11px",
-    display: "flex",
-    flexDirection: "row",
-    columnGap: "34px",
-
-    "& > div > h2 > span": {
-      color: "#0094FF",
+        "& > span": {
+          color: "#0094FF",
+        }
+      },
+  
+      "& > h2:nth-of-type(2)" : {
+        margin: 0,
+        marginTop: "7px",
+        whiteSpace: "nowrap",
+        fontWeight: 700,
+        fontSize: "15px",
+        lineHeight: "23px",
+        letterSpacing: "-1px"    
     },
-
-    "& > div > img": {
-      width: "28px",
-      height: "31.99px",
-    },
-
-    "& > div > h2": {  
-      margin: 0,
-      fontWeight: 700,
-      fontSize: "26px",
-      lineHeight: "32px",
-      letterSpacing: "-1px",
-      color: "#363636",
-    },
-
-    "& > div: nth-of-type(1)": {
-      width: "66px",
-      height: "28px",
-      fontFamily: "AlongSanss2",
-      fontWeight: 800,
-      fontSize: "20.4836px",
-      lineHeight: "23px",
-      letterSpacing: "-0.731557px",
-      color: "#363636",
-      borderBottom: "4px solid #363636",
-    },
-
-    "& > div: nth-of-type(2)": {
-      display: "flex",
-      justifyContent: "flex-start",
-      fontFamily: "NotoSansKR",
-      fontWeight: 700,
-      fontSize: "26px",
-      lineHeight: "32px",
-      letterSpacing: "-1px",
     }
-  },
-
-  image: {
-    paddingTop: "57px",
-  },
-  }
+}
 
 }));
 
