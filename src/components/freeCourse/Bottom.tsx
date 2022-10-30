4c9361ed47aab24c1f988f0d27ae7997202de5b3
JunPyoL22 <jplee031107@gmail.com>
2022-10-30 23:58:24 +0900

    Feat: 무료강의 페이지 하단 컴포넌트 추가

---
+++ src/components/freeCourse/Bottom.tsx
@@ -0,0 +1,53 @@
+
+import { Title } from '@mantine/core';
+import useStyles from './bottomStyle';
+
+const Bottom = () => {
+  const {classes} = useStyles();
+
+  const contentInWordBalloon = [
+    ["개발자 취업 고민", "멘토에게 물어보기", require("../../static/image/logo/SlackC.png")],
+    ["결과물 피드백", "받아보기", require("../../static/image/logo/SlackC.png")],
+    ["로드맵, 수업", "문의하기", require("../../static/image/logo/NaverCafe.png")],
+    ["1:1", "문의하기", require("../../static/image/logo/KakaoTalk.png")],
+  ]
+  const WordBalloon = (titleTop: string, titleBottom: string, iconPath: string) => (
+    <div key={titleTop+titleBottom} className={classes.wordBalloon}>
+      <Title className={classes.wordBallonTitle}>
+        {titleTop}
+        <br/>
+        {titleBottom}
+      </Title>
+      <img className={classes.icon} src={iconPath} alt={iconPath} />
+    </div>
+  );
+
+  return (
+    <div className={classes.main}>
+      <div className={classes.content}>
+        <div className={classes.contentLeft}>
+          <Title className={classes.category}>SUPER COMMUNITY</Title>
+          <Title className={classes.title}>
+            24시간
+            <br />
+            슈코와 얘기하세요.
+          </Title>
+        </div>
+        <div className={classes.contentRight}>
+          <div className={classes.wordBalloons}>
+            {
+              contentInWordBalloon.map(
+                (each) => {
+                  const [titleTop, titleBottom, iconPath] = each;
+                  return WordBalloon(titleTop, titleBottom, iconPath);
+                }
+              )
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Bottom;
