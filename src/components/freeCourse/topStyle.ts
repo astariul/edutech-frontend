8e495e632dd2a84325d5278fda677594592c01e2
JunPyoL22 <jplee031107@gmail.com>
2022-10-31 19:26:58 +0900

    Feat: 무료강의 페이지 상단 컴포넌트 모바일 디자인 시안 적용

--- src/components/freeCourse/topStyle.ts
+++ src/components/freeCourse/topStyle.ts
@@ -7,7 +7,7 @@ const useStyles = createStyles((theme) => ({
     backgroundSize: "cover",
     height: "599px",
 
-    [`@media (max-width: 850px)`]: {
+    [`@media (max-width: 900px)`]: {
       marginTop: "0px",
       height: "564px",
     }
@@ -29,13 +29,14 @@ const useStyles = createStyles((theme) => ({
     fontFamily: "NotoSansKR",
     paddingTop: "141px",
 
-    [`@media (max-width: 850px)`]: {
+    [`@media (max-width: 900px)`]: {
       width: "375px",
       margin: "0 auto",
-      paddingTop: "181px"
+      paddingTop: "183px"
     }
   },
 
+  
   TopMessageSmall: {
     textAlign: "center",
     fontWeight: 700,
@@ -43,23 +44,51 @@ const useStyles = createStyles((theme) => ({
     lineHeight: "35px",
     letterSpacing: "-0.04em",
     color: "#FFFFFF",
-
-    "& > span": {
+    
+    span: {
       color: "#DBFF00",
+    },
+
+    [`@media (max-width: 900px)`]: {
+      display: "none",
     }
   },
-  
+
+  TopMessageSmallMobile: {
+    display: "none",
+    
+    [`@media (max-width: 900px)`]: {
+      display: "block",
+      textAlign: "center",
+      fontWeight: 700,
+      color: "#DBFF00",
+      fontSize: "16px",
+      lineHeight: "25px",
+      letterSpacing: "-0.07em",
+    }
+  },
+
   TopMessageLarge: {
     paddingTop: "43px",
     textAlign: "center",
     color: "#FFFFFF",
     
+    [`@media (max-width: 900px)`]: {
+      paddingTop: "13px",
+    },
+
     "h2": {
       margin: 0,
       letterSpacing: "-0.06em",
       fontWeight: 900,
       fontSize: "77.0008px",
       lineHeight: "82px",
+
+      [`@media (max-width: 900px)`]: {
+        fontSize: "42px",
+        lineHeight: "65px",
+        letterSpacing: "-0.07em",
+      }
     },
 
     "span": {
@@ -70,6 +99,12 @@ const useStyles = createStyles((theme) => ({
       color: "#DBFF00",
       fontSize: "128px",
       lineHeight: "37px",
+
+      [`@media (max-width: 900px)`]: {
+        fontSize: "90px",
+        lineHeight: "37px",
+        letterSpacing: "-2px",
+      }
     }
   },
 
