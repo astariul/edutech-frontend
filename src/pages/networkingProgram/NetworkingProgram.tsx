fb29562d2f4eb7ea01760329d4bdeb0476d48204
김 동욱 kdw <kdw@spacewalk.tech>
2022-11-02 13:08:54 +0900

    feat: Sub2Section 추가

--- src/pages/networkingProgram/NetworkingProgram.tsx
+++ src/pages/networkingProgram/NetworkingProgram.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import MainSection from "../../components/networkingProgram/MainSection";
 import Sub1Section from "../../components/networkingProgram/Sub1Section";
+import Sub2Section from "../../components/networkingProgram/Sub2Section";
 
 const NetworkingProgram: FC = () => {
   return (
     <section>
       <MainSection />
       <Sub1Section />
+      <Sub2Section />
     </section>
   );
 };
