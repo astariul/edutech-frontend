ca133a18a3f4f6d2653300538fafa76c388fedf8
lkylove23 <lkylove2323@gmail.com>
2022-11-07 00:51:51 +0900

    Feat: 이력서페이지 모바일 반응형 적용

--- src/pages/resume/style.ts
+++ src/pages/resume/style.ts
@@ -3,28 +3,42 @@ import { createStyles } from "@mantine/core";
 export const useStyles = createStyles((theme, _, getRef) => ({
   bgSection: {
     position: "relative",
-    backgroundImage: "url(/image/resume/resumeMain.png)",
-    backgroundSize: "cover",
-    height: "599px ",
-    // height: "50vh",
-    //To-do 배경 이미지 사이즈 좀더 조절 하면 좋을것 같음
-    width: "100vw",
 
-    [`@media (max-width: 900px)`]: {
+    backgroundSize: "cover",
+
+    [`@media (min-width: 481px)`]: {
+      backgroundImage: "url(/image/resume/resumeBG.png)",
+      height: "599px ",
+      // height: "50vh",
+      //To-do 배경 이미지 사이즈 좀더 조절 하면 좋을것 같음
+      width: "100vw",
+    },
+
+    [`@media (max-width: 480px)`]: {
+      backgroundImage: "url(/image/resume/resumeBGMobile.png)",
       marginTop: "0px",
       height: "564px",
     },
   },
+
   mainMessage: {
     fontFamily: "Noto Snas KR",
-    fontSize: "77px",
+
     fontWeight: "bolder",
     lineHeight: "90px",
     letterSpacing: "-0.06em",
-    paddingTop: "270px",
+
     alignItems: "center",
     textAlign: "center",
     color: "white",
+    [`@media (min-width: 481px)`]: {
+      fontSize: "77px",
+      paddingTop: "270px",
+    },
+    [`@media (max-width: 480px)`]: {
+      fontSize: "33px",
+      paddingTop: "200px",
+    },
   },
   card: {
     width: "368px",
