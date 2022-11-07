d7a845aca33285622831e59b4679fdfd6dd0e375
JunPyoL22 <jplee031107@gmail.com>
2022-11-07 09:32:28 +0900

    Fix: 정현수 엔지니어 추천 커맨트 컴포넌트 추가(사진, 이름 익명처리)

--- src/components/product/BodyProgram.tsx
+++ src/components/product/BodyProgram.tsx
@@ -55,7 +55,7 @@ const SuperMessage = ({mainMessage, subMessage}: SuperMessageProps) => {
 }
 
 interface MentorCardProps {
-  image: string;
+  image?: string;
   nameAndPosition: string;
   company: string;
   comment: string | ReactNode;
@@ -67,7 +67,7 @@ const MentorCard = ({image, nameAndPosition, company, comment}: MentorCardProps)
     <div className={cx(classes.rowFlex, classes.mentorCard)}>
       <div className={cx(classes.rowFlex, classes.mentorCardInner)}>
         <div className="mentorCardMentorImage">
-          <img src={image} alt="mentorImage" />
+          {image === undefined ? <></> : <img src={image} alt="mentorImage" />}
         </div>
         <div className={cx(classes.columnFlex, "mentorCardMentorInfo")}>
           <div className={cx(classes.columnFlex, "mentorCardMentorNameAndPosition")}>
@@ -246,6 +246,14 @@ const BodyProgram = () => {
             />
             <Divider size={42} style={{borderTopColor: "transparent"}}/>
             <div className={cx(classes.rowFlex, classes.cardArea)}>
+              <MentorCard
+                nameAndPosition="정OO Engineer"
+                company= ""
+                comment={<p>취업 시 회사가 지원자에 대해 아무래도 정보가 부족하여 고민을 많이 하게 되는
+                          <br />데 지원자의 강점을 알려 줄 수 있기에 좋았습니다. 저에 대해서 회사에 객관적
+                          <br />으로 어필하기에 좋은 면이 있었고 도움이 많이 되었습니다.</p>
+                }
+              />
               <MentorCard
                 image={require("../../static/image/product/mentors/cj.png")}
                 nameAndPosition="손인석 Engineer"
@@ -441,6 +449,22 @@ const BodyProgram = () => {
             />
             <Divider size={42} style={{borderTopColor: "transparent"}}/>
             <div className={cx(classes.columnFlex, classes.cardArea)}>
+            <MentorCard
+                image={require("../../static/image/product/mentors/cj.png")}
+                nameAndPosition="정OO Engineer"
+                company=""
+                comment={
+                  <p>
+                    취업할 때 회사가 저에 대해 아무래도
+                    <br />정보가 부족하여 고민을 많이
+                    <br />하게 될텐데 제 강점을 알려
+                    <br />줄 수 있었습니다. 그리고
+                    <br />회사에 객관적으로 어필하기에
+                    <br />좋은 면이 있었고 취업에
+                    <br />도움이 많이 되었습니다.
+                  </p>
+                }
+              />
               <MentorCard
                 image={require("../../static/image/product/mentors/cj.png")}
                 nameAndPosition="손인석 Engineer"
